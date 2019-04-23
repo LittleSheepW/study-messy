b00836c494b90af8e418594843adf77a6d396f79
SunZhongWei <littlesheepw@gmail.com>
2019-04-23 18:29:47 +0800

    为什么使用SLF4J？

--- src/main/java/com/ww/study/studySlf4j/StudySlf4j.java
+++ src/main/java/com/ww/study/studySlf4j/StudySlf4j.java
@@ -7,6 +7,12 @@ import lombok.extern.slf4j.Slf4j;
  * @Description: 学习使用Slf4j记录日志
  * 加入@Slf4j注解后，实际上会编译为
  * private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LogExample.class);
+ *
+ * 为什么使用SLF4J而不是直接使用Log4j或者Logback？
+ * 1、SLF4J，即简单日志门面（Simple Logging Facade for Java），他不是具体的日志解决方案，它只是服务于各种各样的日志系统。
+ * 说白了SLF4J是一个日志抽象层，允许你使用任何一个日志系统，并且可以随时切换还不需要动已经写好的程序。
+ * 2、简洁的占位符，降低了代码中字符串连接次数，而且还节省了新建的String对象。
+ *
  * @Author: Sun
  * @Create: 2019-04-23 17:01
  * @Version: 1.0
